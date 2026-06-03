82791fb778a1f1b608c6677e9403456f18d07d8c
Sudhir <rihdus.io+dev@gmail.com>
2026-06-03 17:43:46 +0800

    deep clone imple

--- frontend/deep-clone/deep-clone.js
+++ frontend/deep-clone/deep-clone.js
@@ -4,5 +4,32 @@
  * @return {T}
  */
 export default function deepClone(value) {
-  throw 'Not implemented!';
+  const isPrimitive =
+    typeof value == "string" ||
+    typeof value == "boolean" ||
+    typeof value == "number";
+
+  // return primitive values, functions as-is
+  if (
+    isPrimitive ||
+    typeof value == "function" ||
+    value == null ||
+    value == undefined
+  )
+    return value;
+
+  // Array type
+  if (Array.isArray(value)) {
+    return value.map(deepClone);
+  }
+
+  // Object
+  if (typeof value == "object") {
+    return Object.entries(value).reduce((clonedObj, [key, childValue]) => {
+      clonedObj[key] = deepClone(childValue);
+      return clonedObj;
+    }, Object.getPrototypeOf(value));
+  }
+
+  return value
 }
