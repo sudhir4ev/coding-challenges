1158aa342192b16c6d8250aebfd51b8e916e9966
Sudhir <rihdus.io+dev@gmail.com>
2026-06-05 15:04:13 +0800

    map-async-limit impl

---
+++ frontend/map-async-limit/map-async-limit.js
@@ -0,0 +1,37 @@
+/**
+ * @param {Array<unknown>} iterable
+ * @param {(value: unknown) => Promise<unknown>} callbackFn
+ * @param {number} [size=Infinity]
+ *
+ * @return {Promise<Array<unknown>>}
+ */
+export default async function mapAsyncLimit(iterable, callbackFn, size) {
+  const batches = size ? _makeBatches(iterable, size) : [iterable];
+  let results = [];
+
+  console.log(iterable, batches);
+
+  for (const batch of batches) {
+    const batchResults = await Promise.all(
+      batch.map((arg) => callbackFn.apply(this, [arg])),
+    );
+    results = [...results, ...batchResults];
+  }
+
+  console.log(results);
+  return results;
+}
+
+function _makeBatches(iterable, size) {
+  const batches = [];
+  for (
+    let index = 0, batchIndex = 0;
+    index < iterable.length;
+    index += size, batchIndex++
+  ) {
+    const batch = Array.prototype.slice.call(iterable, index, index + size);
+    batches[batchIndex] = batch;
+  }
+
+  return batches;
+}
