b720205a6a3943af372d9832e5b8e22e55524cf6
Sudhir <rihdus.io+dev@gmail.com>
2026-06-09 00:36:15 +0800

    fix array reachable end impl

--- challenges/array-reachable-end/array-reachable-end.js
+++ challenges/array-reachable-end/array-reachable-end.js
@@ -3,17 +3,19 @@
  * @return {boolean}
  */
 export default function arrayReachableEnd(numbers) {
-  let numMoves = 0;
-  let index = 0;
+  let maxReachable = 0;
 
-  if (numbers.length == 0 || numbers.length == 1) return true;
+  for (let i = 0; i < numbers.length; i++) {
+    if (i > maxReachable) {
+      return false;
+    }
 
-  while (numMoves != 0 && index <= numbers.length - 1) {
-    numMoves = numbers[index];
-    index += numMoves;
+    maxReachable = Math.max(maxReachable, i + numbers[i]);
 
-    if (index >= numbers.length - 1) return true;
+    if (maxReachable >= numbers.length - 1) {
+      return true;
+    }
   }
 
-  return false;
+  return true;
 }
