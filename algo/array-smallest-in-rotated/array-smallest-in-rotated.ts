dc8ab8629a674250adc0a61dd056fd56082a5f5c
Sudhir <rihdus.io+dev@gmail.com>
2026-06-15 19:37:11 +0800

    cleanup smallest in rotated

--- algo/array-smallest-in-rotated/array-smallest-in-rotated.ts
+++ algo/array-smallest-in-rotated/array-smallest-in-rotated.ts
@@ -9,44 +9,24 @@ export default function smallestInRotatedArray(numbers: number[]) {
 
   console.log("smallestInRotatedArray", numbers);
 
-  let iter = 0;
-
   while (start < end) {
-    if (iter++ > 100) throw new Error("Potential infinite loop");
+    // Only 2 elements
+    if(start + 1 == end) return Math.min(smallest, numbers[start], numbers[end])
+    
     let mid = Math.trunc((start + end) / 2);
 
     smallest = Math.min(smallest, numbers[mid]);
 
-    if(start + 1 == end) {
-      return Math.min(smallest, numbers[start], numbers[end])
-    }
 
     // array is sorted - smallest number in start position
-    if (numbers[mid] >= numbers[start] && numbers[mid] <= numbers[end]) {
-      console.log(
-        [start, mid, end],
-        `Found el at ${start} - Iter#${iter}`,
-      );
+    if (numbers[mid] >= numbers[start] && numbers[mid] <= numbers[end]) 
       return numbers[start];
-    }
 
     // smallest el in first half
-    if (numbers[mid] < numbers[start]) {
-      console.log(
-        [start, mid, end],
-        `El in first half [${start} - ${mid}] - Iter#${iter}`,
-      );
-      end = mid;
-    }
+    if (numbers[mid] < numbers[start]) end = mid;
 
     // smallest el in second half
-    else {
-      console.log(
-        [start, mid, end],
-        `El in 2nd half [${mid} - ${end}] - Iter$#${iter}`,
-      );
-      start = mid;
-    }
+    else start = mid;
   }
 
   return smallest;
