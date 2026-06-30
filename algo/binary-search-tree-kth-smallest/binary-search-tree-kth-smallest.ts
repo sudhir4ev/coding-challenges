852ecda8797d3ad52199f888b31151c2b6aa3941
Sudhir <rihdus.io+dev@gmail.com>
2026-06-30 17:18:09 +0530

    impl: bst kth smallest el

--- algo/binary-search-tree-kth-smallest/binary-search-tree-kth-smallest.ts
+++ algo/binary-search-tree-kth-smallest/binary-search-tree-kth-smallest.ts
@@ -3,8 +3,24 @@
  * @param {number} k
  * @return {number}
  */
-export default function kthSmallestElementInABst(root: TreeNode | null, k: number) {
-  throw 'Not implemented!';
+export default function kthSmallestElementInABst(
+  root: TreeNode | null,
+  k: number,
+) {
+  /**
+   * traverse left side of the tree until no more leafs,
+   */
+  const results = inOrderBSTWalker(root, [], k);
+  return results.pop();
+}
+
+function inOrderBSTWalker(root: TreeNode | null, result: number[], k: number) {
+  if (root == null) return result;
+  inOrderBSTWalker(root.left, result, k);
+  if (result.length == k) return result;
+  else result.push(root.val);
+  inOrderBSTWalker(root.right, result, k);
+  return result;
 }
 
 export class TreeNode {
