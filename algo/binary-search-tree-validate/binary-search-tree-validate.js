802683fd66582218a41fc2f1372de0f7e9acea14
Sudhir <rihdus.io@gmail.com>
2026-07-01 08:46:59 +0530

    version(bst-validate): first version works

--- algo/binary-search-tree-validate/binary-search-tree-validate.js
+++ algo/binary-search-tree-validate/binary-search-tree-validate.js
@@ -3,5 +3,46 @@
  * @return {boolean}
  */
 export default function binarySearchTreeValidate(root) {
-  throw 'Not implemented!';
+  let isValid = true;
+
+  /**
+ *    10
+     /  \
+    5    15
+        /  \
+       6    20
+      /  \
+     3    7
+ */
+  function bstChecker(root, lmin, lmax) {
+    if (!root) return;
+
+    if (root.left) {
+      if (
+        root.left.val >= root.val ||
+        (lmin != null && root.left.val <= lmin) ||
+        (lmax != null && root.left.val >= lmax)
+      ) {
+        isValid = false;
+        return;
+      }
+      bstChecker(root.left, lmin, root.val);
+    }
+
+    if (root.right) {
+      if (
+        root.right.val <= root.val ||
+        (lmin != null && root.right.val <= lmin) ||
+        (lmax != null && root.right.val >= lmax)
+      ) {
+        isValid = false;
+        return;
+      }
+      bstChecker(root.right, root.val, lmax);
+    }
+  }
+
+  bstChecker(root, null, null);
+
+  return isValid;
 }
