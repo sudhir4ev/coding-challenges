a6dec396e819829643f1dddc7042c5e464a713ca
Sudhir <rihdus.io@gmail.com>
2026-07-02 11:23:10 +0530

    fix: no div requirement

--- algo/array-product-excluding-current/array-product-excluding-current.js
+++ algo/array-product-excluding-current/array-product-excluding-current.js
@@ -4,6 +4,7 @@
  */
 export default function arrayProductExcludingCurrent(numbers) {
   const zeroIndex = [];
+  
   numbers.forEach((num, index) => {
     if (num == 0) {
       zeroIndex.push(index);
@@ -11,23 +12,48 @@ export default function arrayProductExcludingCurrent(numbers) {
   });
 
   if (zeroIndex.length > 1) {
+    // all elements in resutl 0
     return new Array(numbers.length).fill(0);
   } else if (zeroIndex.length == 1) {
     const arr = new Array(numbers.length).fill(0);
 
     const index = zeroIndex[0];
+    // add all index with 0's index set to 1
     numbers.splice(index, 1, 1);
-    arr[index] = arrayMult(numbers);
+    arr[index] = arrayMult(0);
 
     return arr;
-  } else {
-    const total = arrayMult(numbers);
-    return numbers.map((num) => total / num);
   }
-}
 
-function arrayMult(arr) {
-  return arr.reduce((total, num) => {
-    return (total *= num);
-  }, 1);
+  // find product for each index individually
+  const result = [];
+  for (const indexStr in numbers) {
+    const index = Number(indexStr);
+    result[index] = 1;
+
+    if (index == 0) {
+      result[index] *= arrayMult(1);
+      continue;
+    }
+
+    if (index == numbers.length - 1) {
+      result[index] *= arrayMult(0, numbers.length - 2);
+      continue;
+    }
+
+    result[index] = arrayMult(0, index - 1) * arrayMult(index + 1);
+  }
+
+  return result;
+
+  function arrayMult(start, end) {
+
+    let product = 1;
+    end = end ?? numbers.length - 1;
+
+    for (let i = start; i <= end; i++) {
+      product *= numbers[i];
+    }
+    return product;
+  }
 }
