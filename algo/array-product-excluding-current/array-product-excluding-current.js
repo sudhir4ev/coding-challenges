6865cd968f2b5d4f7ac5b2f7769eb8743d71cb89
Sudhir <rihdus.io@gmail.com>
2026-07-02 19:06:32 +0530

    imple: version 2

--- algo/array-product-excluding-current/array-product-excluding-current.js
+++ algo/array-product-excluding-current/array-product-excluding-current.js
@@ -3,53 +3,26 @@
  * @return {number[]}
  */
 export default function arrayProductExcludingCurrent(numbers) {
-  const zeroIndex = [];
-  numbers.forEach((num, index) => {
-    if (num == 0) {
-      zeroIndex.push(index);
-    }
-  });
-
-  if (zeroIndex.length > 1) {
-    // all elements in resutl 0
+  if (numbers.filter((num) => num === 0).length > 1) {
     return new Array(numbers.length).fill(0);
-  } else if (zeroIndex.length == 1) {
-    const arr = new Array(numbers.length).fill(0);
-    const index = zeroIndex[0];
-    // add all index with 0's index set to 1
-    numbers.splice(index, 1, 1);
-    arr[index] = arrayMult(0);
+  }
 
-    return arr;
+  const result = [];
+  const fwdMul = [];
+  const revMul = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    fwdMul[i] = (fwdMul[i - 1] ?? 1) * numbers[i];
+    revMul[i] = (revMul[i - 1] ?? 1) * numbers[numbers.length - 1 - i];
   }
 
   // find product for each index individually
-  const result = [];
-  for (const indexStr in numbers) {
-    const index = Number(indexStr);
-    result[index] = 1;
+  numbers.forEach((_, i) => {
+    result[i] = 1;
+    i > 0 && (result[i] *= fwdMul[i - 1] ?? 1);
+    i < numbers.length - 1 &&
+      (result[i] *= revMul[numbers.length - 2 - i] ?? 1);
+  });
 
-    if (index == 0) {
-      result[index] *= arrayMult(1);
-      continue;
-    }
-
-    if (index == numbers.length - 1) {
-      result[index] *= arrayMult(0, numbers.length - 2);
-      continue;
-    }
-
-    result[index] = arrayMult(0, index - 1) * arrayMult(index + 1);
-  }
   return result;
-
-  function arrayMult(start, end) {
-    let product = 1;
-    end = end ?? numbers.length - 1;
-
-    for (let i = end; i >= start; i--) {
-      product *= numbers[i];
-    }
-    return product;
-  }
 }
