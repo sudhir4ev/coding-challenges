c3b33bcb581c829cd4bd80091af436ae4b579578
Sudhir <rihdus.io@gmail.com>
2026-07-03 13:10:58 +0530

    impl: coin-change

--- algo/coin-change/coin-change.ts
+++ algo/coin-change/coin-change.ts
@@ -3,6 +3,18 @@
  * @param {number} target
  * @return {number}
  */
-export default function coinChange(coins: number[], target: number): number {
-  throw 'Not implemented!';
+export default function minimumCoinsForChange(coins: number[], target: number) {
+  if (target == 0) return 0;
+
+  // Infinity value -> unreachable `minCoin` for all sub targets
+  const minCoins = new Array(target + 1).fill(Infinity);
+  minCoins[0] = 0;
+
+  coins.forEach((coin) => {
+    for (let i = coin; i <= target; i++) {
+      minCoins[i] = Math.min(minCoins[i], minCoins[i - coin] + 1);
+    }
+  });
+
+  return minCoins[target] == Infinity ? -1 : minCoins[target];
 }
