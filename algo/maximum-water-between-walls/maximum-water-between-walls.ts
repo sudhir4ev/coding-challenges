0eeb4fd40d78eac82477040672598a76802fa6fe
Sudhir <rihdus.io@gmail.com>
2026-07-04 22:07:40 +0530

    impl: maximum-water-between-walls

--- algo/maximum-water-between-walls/maximum-water-between-walls.ts
+++ algo/maximum-water-between-walls/maximum-water-between-walls.ts
@@ -2,6 +2,32 @@
  * @param {number[]} walls
  * @return {number}
  */
-export default function maximumWaterBetweenWalls(walls: number[]): number {
-  throw 'Not implemented!';
+export default function maximumWaterBetweenWalls(walls: number[]) {
+  /**
+   * start the window from begenning to end of the walls
+   * move shorter of start and end pointers and compare the resulting volume
+   * with true max volume.
+   */
+
+  if (walls.length < 2) return 0;
+
+  let start = 0,
+    end = walls.length - 1,
+    maxVol = 0;
+
+  maxVol = Math.max(maxVol, Math.min(walls[start], walls[end]) * (end - start));
+
+  do {
+    if (walls[start] <= walls[end])
+      // move start pointer forward
+      start++;
+    else
+      // move end pointer backward
+      end--;
+
+    const newVol = Math.min(walls[start], walls[end]) * (end - start);
+    maxVol = Math.max(maxVol, newVol);
+  } while (start < end);
+
+  return maxVol;
 }
